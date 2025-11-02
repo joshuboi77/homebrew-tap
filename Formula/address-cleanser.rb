4e348c6f249e6128b886862fc6637f6989f9764c
Joshua Garza <joshuagarza056@gmail.com>
2025-11-02 01:14:47 -0500

    Update address-cleanser to v1.0.21 - add cmath runtime hook

--- Formula/address-cleanser.rb
+++ Formula/address-cleanser.rb
@@ -1,12 +1,12 @@
 class AddressCleanser < Formula
   desc "Parse, validate, and format US addresses"
   homepage "https://github.com/joshuboi77/address-cleanser"
-  version "1.0.20"
+  version "1.0.21"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/joshuboi77/address-cleanser/releases/download/v1.0.20/address-cleanser-macos-arm64.zip"
-      sha256 "3c36129fa311df46b8b926fd7419dd9ec6e1b4bce7b85d82dcdfe66f230e3438"
+      url "https://github.com/joshuboi77/address-cleanser/releases/download/v1.0.21/address-cleanser-macos-arm64.zip"
+      sha256 "5de7a2b688a80e6c522938fc2680a0609024717c7b049fe6425dae65e1cdcd52"
     else
       url "https://github.com/joshuboi77/address-cleanser/releases/download/v1.0.18/address-cleanser-macos-universal.zip"
       sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
