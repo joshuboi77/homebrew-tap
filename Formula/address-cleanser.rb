6cda9d646ac7fc4aeff5033727f58afe69db7111
Joshua Garza <joshuagarza056@gmail.com>
2025-11-02 00:11:10 -0500

    Update address-cleanser to v1.0.20 - fix cmath and exclude pandas testing

--- Formula/address-cleanser.rb
+++ Formula/address-cleanser.rb
@@ -1,12 +1,12 @@
 class AddressCleanser < Formula
   desc "Parse, validate, and format US addresses"
   homepage "https://github.com/joshuboi77/address-cleanser"
-  version "1.0.19"
+  version "1.0.20"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/joshuboi77/address-cleanser/releases/download/v1.0.19/address-cleanser-macos-arm64.zip"
-      sha256 "369d6abc29ac9de556cf3c89005fe819b1acff1e29cef5f096e579bf3e816e92"
+      url "https://github.com/joshuboi77/address-cleanser/releases/download/v1.0.20/address-cleanser-macos-arm64.zip"
+      sha256 "3c36129fa311df46b8b926fd7419dd9ec6e1b4bce7b85d82dcdfe66f230e3438"
     else
       url "https://github.com/joshuboi77/address-cleanser/releases/download/v1.0.18/address-cleanser-macos-universal.zip"
       sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
