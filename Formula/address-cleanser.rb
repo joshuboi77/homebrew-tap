1df5296a281dfd8e5fc4ce1d338f8cb6f4555c75
Joshua Garza <joshuagarza056@gmail.com>
2025-11-02 01:29:37 -0600

    Update address-cleanser to v1.0.27 - catch cleanup errors at top level
    - Wrap cli() in try/except to catch cleanup errors
    - Suppress base_library.zip errors during shutdown
    - Exit cleanly with no error messages
    - Perfect user experience

--- Formula/address-cleanser.rb
+++ Formula/address-cleanser.rb
@@ -1,12 +1,12 @@
 class AddressCleanser < Formula
   desc "Parse, validate, and format US addresses"
   homepage "https://github.com/joshuboi77/address-cleanser"
-  version "1.0.26"
+  version "1.0.27"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/joshuboi77/address-cleanser/releases/download/v1.0.26/address-cleanser-macos-arm64.zip"
-      sha256 "42e7a38bfa1a587ef3e25875995caa2bc9c7deb8f7c3fb5b74617e61815e9999"
+      url "https://github.com/joshuboi77/address-cleanser/releases/download/v1.0.27/address-cleanser-macos-arm64.zip"
+      sha256 "e317fc2d581240b957c6437426b5421b051d097fe0b8974123a8db8ae51ff8f6"
     else
       url "https://github.com/joshuboi77/address-cleanser/releases/download/v1.0.18/address-cleanser-macos-universal.zip"
       sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
