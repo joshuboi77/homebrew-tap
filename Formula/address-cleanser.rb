b4839d34be60a231ffdceda6be5da4dc8efdfe4f
Joshua Garza <joshuagarza056@gmail.com>
2025-11-01 23:54:20 -0500

    Update address-cleanser to v1.0.19 - add mmap runtime hook

--- Formula/address-cleanser.rb
+++ Formula/address-cleanser.rb
@@ -1,12 +1,12 @@
 class AddressCleanser < Formula
   desc "Parse, validate, and format US addresses"
   homepage "https://github.com/joshuboi77/address-cleanser"
-  version "1.0.18"
+  version "1.0.19"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/joshuboi77/address-cleanser/releases/download/v1.0.18/address-cleanser-macos-arm64.zip"
-      sha256 "43390bc0d46e705a6eebf6343bc22c4ff118e94a9cc4f3901e83bb63334a91ff"
+      url "https://github.com/joshuboi77/address-cleanser/releases/download/v1.0.19/address-cleanser-macos-arm64.zip"
+      sha256 "369d6abc29ac9de556cf3c89005fe819b1acff1e29cef5f096e579bf3e816e92"
     else
       url "https://github.com/joshuboi77/address-cleanser/releases/download/v1.0.18/address-cleanser-macos-universal.zip"
       sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
