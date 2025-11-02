10e918ba09ab1ac91de9f97d3ad194f00f8065d0
Joshua Garza <joshuagarza056@gmail.com>
2025-11-02 01:53:29 -0600

    Update address-cleanser to v1.0.28 - zero errors for Fiverr

--- Formula/address-cleanser.rb
+++ Formula/address-cleanser.rb
@@ -1,12 +1,12 @@
 class AddressCleanser < Formula
   desc "Parse, validate, and format US addresses"
   homepage "https://github.com/joshuboi77/address-cleanser"
-  version "1.0.27"
+  version "1.0.28"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/joshuboi77/address-cleanser/releases/download/v1.0.27/address-cleanser-macos-arm64.zip"
-      sha256 "e317fc2d581240b957c6437426b5421b051d097fe0b8974123a8db8ae51ff8f6"
+      url "https://github.com/joshuboi77/address-cleanser/releases/download/v1.0.28/address-cleanser-macos-arm64.zip"
+      sha256 "1733177408ee89fbb49fe97a0784dd033ff1a6bd587a85eb6cd23c4fcf4fed2f"
     else
       url "https://github.com/joshuboi77/address-cleanser/releases/download/v1.0.18/address-cleanser-macos-universal.zip"
       sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
